27a0422e81dd1e2f9ec6d6e5b50362df2544f555
DaShork <nhatminhvo2311@gmail.com>
2026-05-12 08:40:39 +0700

    feat(dependency): import web start dependency for this project

--- Week1/01. Getting-Started/RNCourse/App.js
+++ Week1/01. Getting-Started/RNCourse/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello em Khôi Đẹp Trai</Text>
+      <Text>Chào em Khôi nha</Text>
       <StatusBar style="auto" />
     </View>
   );
