0783a04e7e6217d1581ba51f703adb38f7267647
DaShork <nhatminhvo2311@gmail.com>
2026-05-17 14:46:23 +0700

    feat(rename, create and ini assignment folder structure): rename week1 to RN myseft for selflearn, create MMA folder for MMAsubject, initial folder structure for assignment 1

---
+++ MMA301/Assignment1_TodoList/src/constants/colors.ts
@@ -0,0 +1,19 @@
+export const COLORS = {
+  background: "#F5F7FB",
+
+  primary: "#4D81FF",
+
+  cardBlue: "#C7D2FE",
+  cardYellow: "#FDE68A",
+  cardPink: "#F9A8D4",
+  cardGreen: "#A7F3D0",
+
+  text: "#111827",
+  subText: "#6B7280",
+
+  white: "#FFFFFF",
+
+  border: "#E5E7EB",
+
+  danger: "#EF4444",
+};
